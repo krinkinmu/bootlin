5f7083d1e2b1b9baac9e9ce99ccd10b7f717bef3
Mike Krinkin <krinkin.m.u@gmail.com>
2020-08-15 21:58:48 +0100

    Implement basic configuration and data transfer routines for MPSSE
    I only picked implemented functions that might be need to implement
    I2C over MPSSE, so the implemented function are by no means complete
    or generic.
    
    Also note that at this point I didn't really test any of them, so
    likely more changes to the implementation will follow up.
    
    Along the way I changed a little bit the structure of the code to
    introduce mpsse_io_buffer and mpsse_cmd structures. mpsse_io_buffer
    is not a new concept, before this change struct mpsse already
    contained a command buffer. This change generalizes this concept
    to also manage a buffer for data read from the MPSSE.
    
    mpsse_cmd is just a handler that is used to provide access to the
    data read as part of executing a particular command. For each
    command we need to know how big the command is (how many bytes we
    need to write for the command to MPSSE) and how big the response
    is (how many bytes we need to read back from MPSSE as a response
    to the command).
    
    At this point the library doesn't have any reasonable device error
    handling, but that should be bearable since the end goal is to
    have the Linux Kernel driver instead of a userspace tool. That
    being said I do intend to reuse the architecutre in the Linux
    Kernel driver implementation as well where error handling is not
    optional.

--- ftdi/usr/mpsse.c
+++ ftdi/usr/mpsse.c
@@ -5,9 +5,112 @@
 #include <stdlib.h>
 #include <string.h>
 
-static const int mpsse_cmd_buffer_size = 16384;
 
-static int mpsse_setup(struct mpsse *mpsse)
+void mpsse_io_buffer_setup(struct mpsse_io_buffer *io)
+{
+	io->cmd = NULL;
+	io->cmd_size = 0;
+	io->cmd_capacity = 0;
+	io->data = NULL;
+	io->data_size = 0;
+	io->data_capacity = 0;
+}
+
+void mpsse_io_buffer_reset(struct mpsse_io_buffer *io)
+{
+	io->cmd_size = 0;
+	io->data_size = 0;
+}
+
+void mpsse_io_buffer_release(struct mpsse_io_buffer *io)
+{
+	free(io->cmd);
+	free(io->data);
+}
+
+void *mpsse_cmd(struct mpsse_cmd *cmd)
+{
+	assert(cmd);
+	assert(cmd->io);
+	assert(cmd->io->cmd);
+	return (unsigned char *)cmd->io->cmd + cmd->cmd_offset;
+}
+
+void *mpsse_data(struct mpsse_cmd *cmd)
+{
+	assert(cmd);
+	assert(cmd->io);
+	assert(cmd->io->data);
+	return (unsigned char *)cmd->io->data + cmd->data_offset;
+}
+
+static unsigned mpsse_io_buffer_reserve_cmd(
+	struct mpsse_io_buffer *io, unsigned size)
+{
+	const unsigned offset = io->cmd_size;
+
+	if (io->cmd_size + size > io->cmd_capacity) {
+		unsigned new_capacity = 2 * io->cmd_capacity / 3;
+		void *new_cmd;
+
+		if (new_capacity < io->cmd_size + size)
+			new_capacity = io->cmd_size + size;
+
+		assert((new_cmd = realloc(io->cmd, new_capacity)) != NULL);
+		io->cmd = new_cmd;
+		io->cmd_capacity = new_capacity;
+	}
+
+	io->cmd_size += size;
+	return offset;
+}
+
+static unsigned mpsse_io_buffer_reserve_data(
+	struct mpsse_io_buffer *io, unsigned size)
+{
+	const unsigned offset = io->data_size;
+
+	if (io->data_size + size > io->data_capacity) {
+		unsigned new_capacity = 2 * io->data_capacity / 3;
+		void *new_data;
+
+		if (new_capacity < io->data_size + size)
+			new_capacity = io->data_size + size;
+
+		assert((new_data = realloc(io->data, new_capacity)) != NULL);
+		io->data = new_data;
+		io->data_capacity = new_capacity;
+	}
+
+	io->data_size += size;
+	return offset;
+}
+
+int mpsse_open(const struct serial *serial, struct mpsse *mpsse)
+{
+	FT_HANDLE handle;
+
+	if (ftdi_open(serial, &handle) != 0)
+		return -1;
+
+	mpsse->handle = handle;
+	if (mpsse_reset(mpsse) != 0) {
+		mpsse_close(mpsse);
+		return -1;
+	}
+
+	return 0;
+}
+
+int mpsse_close(struct mpsse *mpsse)
+{
+	int ret = ftdi_close(mpsse->handle);
+
+	memset(mpsse, 0, sizeof(*mpsse));
+	return ret;
+}
+
+int mpsse_reset(struct mpsse *mpsse)
 {
 	if (ftdi_reset(mpsse->handle) != 0)
 		return -1;
@@ -27,87 +130,260 @@ static int mpsse_setup(struct mpsse *mpsse)
 	return 0;
 }
 
-int mpsse_flush(struct mpsse *mpsse)
+static void mpsse_cmd_prepare(
+	struct mpsse_io_buffer *io,
+	struct mpsse_cmd *cmd,
+	unsigned cmd_size,
+	unsigned data_size)
 {
-	if (mpsse->cmd_offset == 0)
-		return 0;
-
-	if (ftdi_write_exactly(
-		mpsse->handle, mpsse->cmd_buffer, mpsse->cmd_offset) < 0)
-		return -1;
-	mpsse->cmd_offset = 0;
-	return 0;
+	cmd->io = io;
+	cmd->cmd_offset = mpsse_io_buffer_reserve_cmd(io, cmd_size);
+	cmd->cmd_size = cmd_size;
+	cmd->data_offset = mpsse_io_buffer_reserve_data(io, data_size);
+	cmd->data_size = data_size;
 }
 
-int mpsse_queue(struct mpsse *mpsse, unsigned char cmd)
+static void mpsse_incorrect_command(
+	struct mpsse_io_buffer *io, unsigned char op, struct mpsse_cmd *out)
 {
-	mpsse->cmd_buffer[mpsse->cmd_offset++] = cmd;
-	if (mpsse->cmd_offset == mpsse->cmd_size)
-		return mpsse_flush(mpsse);
-	return 0;
-}
+	struct mpsse_cmd cmd;
 
+	mpsse_cmd_prepare(io, &cmd, 1, 2);
+	*((unsigned char *)mpsse_cmd(&cmd)) = op;
+	if (out)
+		*out = cmd;
+}
 
 int mpsse_verify(struct mpsse *mpsse)
 {
-	unsigned char buf[2];
+	struct mpsse_io_buffer io;
+	struct mpsse_cmd cmd;
+	unsigned char *data;
 
-	assert(mpsse_queue(mpsse, 0xaa) == 0);
-	if (mpsse_flush(mpsse) != 0) {
+	mpsse_io_buffer_setup(&io);
+	mpsse_incorrect_command(&io, 0xaa, &cmd);
+	if (mpsse_submit(mpsse, &io) != 0)
+		goto err;
+
+	data = mpsse_data(&cmd);
+	if (data[0] != 0xfa || data[1] != 0xaa)
+		goto err;
+
+	mpsse_io_buffer_reset(&io);
+	mpsse_incorrect_command(&io, 0xab, &cmd);
+	if (mpsse_submit(mpsse, &io) != 0)
+		goto err;
+
+	data = mpsse_data(&cmd);
+	if (data[0] != 0xfa || data[1] != 0xab)
+		goto err;
+
+	return 0;
+
+err:
+	mpsse_io_buffer_release(&io);
+	return -1;
+}
+
+int mpsse_submit(struct mpsse *mpsse, struct mpsse_io_buffer *io)
+{
+	if (io->cmd_size != 0 &&
+	    ftdi_write_exactly(mpsse->handle, io->cmd, io->cmd_size) < 0)
 		return -1;
-	}
 
-	if (ftdi_read_exactly(mpsse->handle, buf, sizeof(buf)) < 0)
-		return -1;
-
-	if (buf[0] != 0xfa || buf[1] != 0xaa)
-		return -1;
-
-	assert(mpsse_queue(mpsse, 0xab) == 0);
-	if (mpsse_flush(mpsse) != 0)
-		return -1;
-
-	if (ftdi_read_exactly(mpsse->handle, buf, sizeof(buf)) < 0)
-		return -1;
-
-	if (buf[0] != 0xfa || buf[1] != 0xab)
+	// Basically no error handling here. YOLO!
+	if (io->data_size != 0 &&
+	    ftdi_read_exactly(mpsse->handle, io->data, io->data_size) < 0)
 		return -1;
 
 	return 0;
 }
 
-int mpsse_open(const struct serial *serial, struct mpsse *mpsse)
+
+void mpsse_disable_freq_div5(struct mpsse_io_buffer *io, struct mpsse_cmd *out)
 {
-	FT_HANDLE handle;
-	unsigned char *cmd_buffer;
+	struct mpsse_cmd cmd;
 
-	if (ftdi_open(serial, &handle) != 0)
-		return -1;
-
-	cmd_buffer = malloc(mpsse_cmd_buffer_size);
-	if (!cmd_buffer) {
-		ftdi_close(handle);
-		return -1;
-	}
-
-	mpsse->handle = handle;
-	mpsse->cmd_buffer = cmd_buffer;
-	mpsse->cmd_size = mpsse_cmd_buffer_size;
-	mpsse->cmd_offset = 0;
-
-	if (mpsse_setup(mpsse) != 0) {
-		mpsse_close(mpsse);
-		return -1;
-	}
-
-	return 0;
+	mpsse_cmd_prepare(io, &cmd, 1, 0);
+	*((unsigned char *)mpsse_cmd(&cmd)) = 0x8a;
+	if (out)
+		*out = cmd;
 }
 
-int mpsse_close(struct mpsse *mpsse)
-{
-	int ret = ftdi_close(mpsse->handle);
 
-	free(mpsse->cmd_buffer);
-	memset(mpsse, 0, sizeof(*mpsse));
-	return ret;
+void mpsse_disable_adaptive_clocking(
+	struct mpsse_io_buffer *io, struct mpsse_cmd *out)
+{
+	struct mpsse_cmd cmd;
+
+	mpsse_cmd_prepare(io, &cmd, 1, 0);
+	*((unsigned char *)mpsse_cmd(&cmd)) = 0x97;
+	if (out)
+		*out = cmd;
 }
+
+void mpsse_disable_loopback(struct mpsse_io_buffer *io, struct mpsse_cmd *out)
+{
+	struct mpsse_cmd cmd;
+
+	mpsse_cmd_prepare(io, &cmd, 1, 0);
+	*((unsigned char *)mpsse_cmd(&cmd)) = 0x85;
+	if (out)
+		*out = cmd;
+}
+
+void mpsse_enable_3phase_clocking(
+	struct mpsse_io_buffer *io, struct mpsse_cmd *out)
+{
+	struct mpsse_cmd cmd;
+
+	mpsse_cmd_prepare(io, &cmd, 1, 0);
+	*((unsigned char *)mpsse_cmd(&cmd)) = 0x8c;
+	if (out)
+		*out = cmd;
+}
+
+void mpsse_set_drive0_pins(
+	struct mpsse_io_buffer *io, unsigned pinmask, struct mpsse_cmd *out)
+{
+	struct mpsse_cmd cmd;
+	unsigned char *buf;
+
+	mpsse_cmd_prepare(io, &cmd, 3, 0);
+	buf = mpsse_cmd(&cmd);
+	buf[0] = 0x9e;
+	buf[1] = pinmask & 0xff;
+	buf[2] = (pinmask >> 8) & 0xff;
+	if (out)
+		*out = cmd;
+}
+
+void mpsse_set_freq_divisor(
+	struct mpsse_io_buffer *io, unsigned divisor, struct mpsse_cmd *out)
+{
+	struct mpsse_cmd cmd;
+	unsigned char *buf;
+
+	assert(divisor <= 0xffff);
+	mpsse_cmd_prepare(io, &cmd, 3, 0);
+	buf = mpsse_cmd(&cmd);
+	buf[0] = 0x86;
+	buf[1] = divisor & 0xff;
+	buf[2] = (divisor >> 8) & 0xff;
+	if (out)
+		*out = cmd;
+}
+
+void mpsse_set_output(
+	struct mpsse_io_buffer *io,
+	unsigned pinmask,
+	unsigned pinvals,
+	struct mpsse_cmd *out)
+{
+	struct mpsse_cmd cmd;
+	unsigned char *buf;
+
+	mpsse_cmd_prepare(io, &cmd, 6, 0);
+	buf = mpsse_cmd(&cmd);
+	buf[0] = 0x80;
+	buf[1] = pinvals & 0xff;
+	buf[2] = pinmask & 0xff;
+	buf[3] = 0x82;
+	buf[4] = pinvals >> 8 & 0xff;
+	buf[5] = (pinmask >> 8) & 0xff;
+	if (out)
+		*out = cmd;
+}
+
+void mpsse_write_bytes(
+	struct mpsse_io_buffer *io,
+	const void *data,
+	unsigned size,
+	struct mpsse_cmd *out)
+{
+	struct mpsse_cmd cmd;
+	unsigned char *buf;
+
+	if (size == 0) {
+		if (out) mpsse_cmd_prepare(io, out, 0, 0);
+		return;
+	}
+
+	assert(size <= 0xffff);
+	mpsse_cmd_prepare(io, &cmd, 3 + size, 0);
+	buf = mpsse_cmd(&cmd);
+	buf[0] = 0x11;
+	buf[1] = (size - 1) & 0xff;
+	buf[2] = ((size - 1) >> 8) & 0xff;
+	memcpy(&buf[3], data, size);
+	if (out)
+		*out = cmd;
+}
+
+void mpsse_read_bytes(
+	struct mpsse_io_buffer *io, unsigned size, struct mpsse_cmd *out)
+{
+	struct mpsse_cmd cmd;
+	unsigned char *buf;
+
+	if (size == 0) {
+		if (out) mpsse_cmd_prepare(io, out, 0, 0);
+		return;
+	}
+
+	assert(size <= 0xffff);
+	mpsse_cmd_prepare(io, &cmd, 4, size);
+	buf = mpsse_cmd(&cmd);
+	buf[0] = 0x20;
+	buf[1] = (size - 1) & 0xff;
+	buf[2] = ((size - 1) >> 8) & 0xff;
+	buf[3] = 0x87;
+	if (out)
+		*out = cmd;
+}
+
+void mpsse_write_bits(
+	struct mpsse_io_buffer *io,
+	unsigned char data,
+	unsigned bits,
+	struct mpsse_cmd *out)
+{
+	struct mpsse_cmd cmd;
+	unsigned char *buf;
+
+	if (bits == 0) {
+		if (out) mpsse_cmd_prepare(io, out, 0, 0);
+		return;
+	}
+
+	assert(bits <= 8);
+	mpsse_cmd_prepare(io, &cmd, 3, 0);
+	buf = mpsse_cmd(&cmd);
+	buf[0] = 0x13;
+	buf[1] = (bits - 1) & 0xff;
+	buf[2] = data;
+	if (out)
+		*out = cmd;
+}
+
+void mpsse_read_bits(
+	struct mpsse_io_buffer *io, unsigned bits, struct mpsse_cmd *out)
+{
+	struct mpsse_cmd cmd;
+	unsigned char *buf;
+
+	if (bits == 0) {
+		if (out) mpsse_cmd_prepare(io, out, 0, 0);
+		return;
+	}
+
+	assert(bits <= 8);
+	mpsse_cmd_prepare(io, &cmd, 3, 1);
+	buf = mpsse_cmd(&cmd);
+	buf[0] = 0x22;
+	buf[1] = (bits - 1) & 0xff;
+	buf[2] = 0x87;
+	if (out)
+		*out = cmd;
+}
+
--- ftdi/usr/mpsse.h
+++ ftdi/usr/mpsse.h
@@ -4,17 +4,114 @@
 
 #include "ftdi.h"
 
+// MPSSE IO buffer just manages memory for the commands we want to send to
+// the MPSSE and for the expected results.
+// 
+// One way to look at the MPSSE IO buffer is that it's a collection of commands
+// that we want to send to the MPSSE in one batch.
+struct mpsse_io_buffer {
+	void *cmd;
+	unsigned cmd_size;
+	unsigned cmd_capacity;
+
+	void *data;
+	unsigned data_size;
+	unsigned data_capacity;
+};
+
+void mpsse_io_buffer_setup(struct mpsse_io_buffer *io);
+void mpsse_io_buffer_reset(struct mpsse_io_buffer *io);
+void mpsse_io_buffer_release(struct mpsse_io_buffer *io);
+
+
 struct mpsse {
 	FT_HANDLE handle;
-	unsigned char *cmd_buffer;
-	int cmd_size;
-	int cmd_offset;
 };
 
 int mpsse_open(const struct serial *serial, struct mpsse *mpsse);
-int mpsse_verify(struct mpsse *mpsse);
-int mpsse_queue(struct mpsse *mpsse, unsigned char cmd);
-int mpsse_flush(struct mpsse *mpsse);
 int mpsse_close(struct mpsse *mpsse);
+
+// Reset will reset the device to it's initial state and drain all the device
+// buffer. The current version of the library doesn't provde many error
+// handling capabilities, so in most cases once you encountered an error you
+// should reset the device and start over.
+int mpsse_reset(struct mpsse *mpsee);
+
+// Checks that MPSSE device is responding to the MPSSE commands as expected.
+int mpsse_verify(struct mpsse *mpsse);
+
+// Executes the commands described by the MPSSE IO buffer. If the function
+// completes successfully it returns 0, otherwise a non-0 value is returned.
+// If the execution fails you should not expect that the device will be in
+// any particular state, so if you want to continue using it you should reset.
+int mpsse_submit(struct mpsse *mpsse, struct mpsse_io_buffer *io);
+
+
+// MPSSE command is a handler that allows access to the command data itself
+// as well as the data that was read from the device if any after the command
+// has been executed.
+//
+// Never create MPSSE command manually and never use MPSSE command after the
+// MPSSE IO buffer associated with it has been reset or released, since the
+// MPSSE IO buffer is managing the memory.
+struct mpsse_cmd {
+	struct mpsse_io_buffer *io;
+	unsigned cmd_offset;
+	unsigned cmd_size;
+	unsigned data_offset;
+	unsigned data_size;
+};
+
+// Accessor function to the MPSSE command buffer as well as to the buffer
+// for the data read as part of executing the command.
+void *mpsse_cmd(struct mpsse_cmd *cmd);
+void *mpsse_data(struct mpsse_cmd *cmd);
+
+// Most of these functions are direct or almost direct wrappers around the
+// MPSSE command set. You can get familiar with the list of the available
+// commands and their meaning through the documentation for MPSSE command
+// processor on the FTDI site.
+// 
+// None of the functions actually executes MPSSE commands, they just update
+// the provided MPSSE IO buffer. To actually execute the commands you should
+// call mpsse_submit function with the MPSSE IO buffer.
+//
+// All of the function take a pointer to MPSSE command structure as the last
+// argument. The pointer may be NULL, but in this case you will not have access
+// to any data read as part of executing the command. If you actually want to
+// read some data from MPSSE then you need to provide a valid MPSSE command
+// pointer as the last argument. The data could be access by using mpsse_data
+// function on the MPSSE command after it has been executed.
+void mpsse_disable_freq_div5(
+	struct mpsse_io_buffer *io, struct mpsse_cmd *cmd);
+void mpsse_disable_adaptive_clocking(
+	struct mpsse_io_buffer *io, struct mpsse_cmd *cmd);
+void mpsse_disable_loopback(
+	struct mpsse_io_buffer *io, struct mpsse_cmd *cmd);
+void mpsse_enable_3phase_clocking(
+	struct mpsse_io_buffer *io, struct mpsse_cmd *cmd);
+void mpsse_set_drive0_pins(
+	struct mpsse_io_buffer *io, unsigned pinmask, struct mpsse_cmd *cmd);
+void mpsse_set_freq_divisor(
+	struct mpsse_io_buffer *io, unsigned divisor, struct mpsse_cmd *cmd);
+void mpsse_set_output(
+	struct mpsse_io_buffer *io,
+	unsigned pinmask,
+	unsigned pinvals,
+	struct mpsse_cmd *cmd);
+void mpsse_write_bytes(
+	struct mpsse_io_buffer *io,
+	const void *data,
+	unsigned size,
+	struct mpsse_cmd *cmd);
+void mpsse_read_bytes(
+	struct mpsse_io_buffer *io, unsigned size, struct mpsse_cmd *cmd);
+void mpsse_write_bits(
+	struct mpsse_io_buffer *io,
+	unsigned char data,
+	unsigned bits,
+	struct mpsse_cmd *cmd);
+void mpsse_read_bits(
+	struct mpsse_io_buffer *io, unsigned bits, struct mpsse_cmd *cmd);
 
 #endif  // __MPSSE_H__
