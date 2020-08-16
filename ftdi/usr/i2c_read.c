a8248eb7db64b06c3964586e33f4528d398ad381
Mike Krinkin <krinkin.m.u@gmail.com>
2020-08-16 18:53:08 +0100

    Make i2c_read tool be able to read Wiichuk State
    This is a somewhat hacky but nonethelss functional example of I2C
    read and write via FTDI MPSSE. I tested it on the Nintendo Wiichuk
    controller and it can read the state.
    
    NOTE: I keep the I2C frequency low because:
     * I use a relatively cheap logic analyser for debugging and it doesn't
       work well with high frequencies
     * FTDI MPSSE doesn't have native support for clock stretching and while
       I was still debugging the code I didn't want to introduce additional
       interference just because the device could not respond fast enough.

--- ftdi/usr/i2c_read.c
+++ ftdi/usr/i2c_read.c
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,6 +10,125 @@
 
 static const unsigned device_vid = 0x0005;
 static const unsigned device_pid = 0x0001;
+
+static int i2c_setup(struct mpsse *mpsse)
+{
+	struct mpsse_io_buffer io;
+	int ret;
+
+	mpsse_io_buffer_setup(&io);
+	mpsse_disable_freq_div5(&io, NULL);
+	mpsse_disable_adaptive_clocking(&io, NULL);
+	mpsse_enable_3phase_clocking(&io, NULL);
+	mpsse_set_drive0_pins(&io, 0x7, NULL);
+	mpsse_disable_loopback(&io, NULL);
+	mpsse_set_freq_divisor(&io, 0xffff, NULL);
+	ret = mpsse_submit(mpsse, &io);
+	mpsse_io_buffer_release(&io);
+	return ret;
+}
+
+static int i2c_idle(struct mpsse *mpsse)
+{
+	struct mpsse_io_buffer io;
+	int ret;
+
+	mpsse_io_buffer_setup(&io);
+	mpsse_set_output(&io, 0x40fb, 0xffff, NULL);
+	ret = mpsse_submit(mpsse, &io);
+	mpsse_io_buffer_release(&io);
+	return ret;
+}
+
+static int i2c_start(struct mpsse *mpsse)
+{
+	struct mpsse_io_buffer io;
+	int ret;
+
+	mpsse_io_buffer_setup(&io);
+	for (int i =-0; i < 5; ++ i)
+		mpsse_set_output(&io, 0x00fb, 0x00fd, NULL);
+	for (int i =-0; i < 5; ++ i)
+		mpsse_set_output(&io, 0x40fb, 0x00fc, NULL);
+	ret = mpsse_submit(mpsse, &io);
+	mpsse_io_buffer_release(&io);
+	return ret;
+}
+
+static int i2c_stop(struct mpsse *mpsse)
+{
+	struct mpsse_io_buffer io;
+	int ret;
+
+	mpsse_io_buffer_setup(&io);
+	for (int i =-0; i < 5; ++ i)
+		mpsse_set_output(&io, 0x00fb, 0x00fc, NULL);
+	for (int i =-0; i < 5; ++ i)
+		mpsse_set_output(&io, 0x00fb, 0x00fd, NULL);
+	for (int i =-0; i < 5; ++ i)
+		mpsse_set_output(&io, 0x40fb, 0xffff, NULL);
+	ret = mpsse_submit(mpsse, &io);
+	mpsse_io_buffer_release(&io);
+	return ret;
+}
+
+static int i2c_send_byte(struct mpsse *mpsse, unsigned char byte)
+{
+	struct mpsse_io_buffer io;
+	struct mpsse_cmd cmd;
+	int ret;
+
+	mpsse_io_buffer_setup(&io);
+	mpsse_write_bytes(&io, &byte, sizeof(byte), NULL);
+	mpsse_set_output(&io, 0x00fb, 0x00fe, NULL);
+	mpsse_read_bits(&io, 1, &cmd);
+	ret = mpsse_submit(mpsse, &io);
+	if (ret == 0) {
+		const unsigned char *data = mpsse_data(&cmd);
+
+		if ((*data & 0x1) != 0x0)
+			ret = -1;
+	}
+	mpsse_io_buffer_release(&io);
+	return ret;
+}
+
+static int i2c_send_addr(struct mpsse *mpsse, unsigned char i2c_addr, int read)
+{
+	const unsigned char byte =
+		read ? ((i2c_addr << 1) | 1) : (i2c_addr << 1);
+	return i2c_send_byte(mpsse, byte);
+}
+
+static int i2c_read_bytes(struct mpsse *mpsse, void *data, unsigned size)
+{
+	struct mpsse_io_buffer io;
+	struct mpsse_cmd cmd;
+	int ret;
+
+	mpsse_io_buffer_setup(&io);
+	mpsse_read_bytes(&io, 1, &cmd);
+	for (unsigned i = 1; i < size; ++i) {
+		mpsse_write_bits(&io, 0x00, 1, NULL);
+		mpsse_set_output(&io, 0x00fb, 0x00fe, NULL);
+		mpsse_read_bytes(&io, 1, NULL);
+	}
+	mpsse_write_bits(&io, 0xff, 1, NULL);
+	mpsse_set_output(&io, 0x00fb, 0x00fe, NULL);
+	ret = mpsse_submit(mpsse, &io);
+	if (ret == 0)
+		memcpy(data, mpsse_data(&cmd), size);
+	mpsse_io_buffer_release(&io);
+	return ret;
+}
+
+static void hexdump(FILE *output, const void *data, unsigned size)
+{
+	const unsigned char *b = data;
+
+	for (unsigned i = 0; i < size; ++i)
+		fprintf(output, "0x%02x ", b[i]);
+}
 
 static void usage(const char *name)
 {
@@ -32,9 +152,14 @@ int main(int argc, char **argv)
 	const char *address = NULL;
 	const char *reg = NULL;
 	const char *len = NULL;
+	char *endptr;
+	void *data;
 
 	struct mpsse mpsse;
 	struct serial s;
+	unsigned i2c_addr;
+	unsigned i2c_reg;
+	unsigned read_size;
 	int opt;
 
 	while ((opt = getopt(argc, argv, "s:a:r:l:h")) != -1) {
@@ -94,14 +219,31 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Expect exactly one -a argument\n");
 		return 1;
 	}
+	i2c_addr = strtoul(address, &endptr, 0);
+	if (*endptr != '\0') {
+		fprintf(stderr, "Failed to parse i2c address %s\n", address);
+		return 1;
+	}
 
 	if (!reg) {
 		fprintf(stderr, "Expect exactly one -r argument\n");
 		return 1;
 	}
+	i2c_reg = strtoul(reg, &endptr, 0);
+	if (*endptr != '\0') {
+		fprintf(stderr, "Failed to parse i2c register %s\n", reg);
+		return 1;
+	}
 	
 	if (!len) {
 		fprintf(stderr, "Expect exactly one -l argument\n");
+		return 1;
+	}
+	read_size = strtoul(len, &endptr, 0);
+	if (*endptr != '\0') {
+		fprintf(stderr,
+			"Failed to parse how many bytes to read: %s\n",
+			len);
 		return 1;
 	}
 
@@ -118,11 +260,43 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	if (mpsse_verify(&mpsse) != 0)
+	if (mpsse_verify(&mpsse) != 0) {
 		fprintf(stderr, "Failed to verify MPSSE mode on %s\n", serial);
-	else
-		fprintf(stdout, "MPSSE mode was actived on %s\n", serial);
+		mpsse_close(&mpsse);
+		return 1;
+	}
 
+	assert((data = malloc(read_size)) != NULL);
+
+	assert(i2c_setup(&mpsse) == 0);
+	assert(i2c_idle(&mpsse) == 0);
+
+	assert(i2c_start(&mpsse) == 0);
+	assert(i2c_send_addr(&mpsse, i2c_addr, 0) == 0);
+	assert(i2c_send_byte(&mpsse, 0xf0) == 0);
+	assert(i2c_send_byte(&mpsse, 0x55) == 0);
+	assert(i2c_stop(&mpsse) == 0);
+
+	assert(i2c_start(&mpsse) == 0);
+	assert(i2c_send_addr(&mpsse, i2c_addr, 0) == 0);
+	assert(i2c_send_byte(&mpsse, 0xfb) == 0);
+	assert(i2c_send_byte(&mpsse, 0x00) == 0);
+	assert(i2c_stop(&mpsse) == 0);
+
+	assert(i2c_start(&mpsse) == 0);
+	assert(i2c_send_addr(&mpsse, i2c_addr, 0) == 0);
+	assert(i2c_send_byte(&mpsse, i2c_reg) == 0);
+	assert(i2c_idle(&mpsse) == 0);
+	assert(i2c_start(&mpsse) == 0);
+	assert(i2c_send_addr(&mpsse, i2c_addr, 1) == 0);
+	assert(i2c_read_bytes(&mpsse, data, read_size) == 0);
+	assert(i2c_stop(&mpsse) == 0);
+
+	fprintf(stdout, "Read data:\n");
+	hexdump(stdout, data, read_size);
+	fprintf(stdout, "\n");
+
+	free(data);
 	if (mpsse_close(&mpsse) != 0) {
 		fprintf(stderr, "Failed to close %s\n", serial);
 		return 1;
